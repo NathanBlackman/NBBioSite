894fb6903eeff3273ff6d0f79e442d8fa625691d
Nathan Blackman <nathanpgblackman@gmail.com>
2023-01-19 21:54:02 -0600

    adding routes

---
+++ src/Router/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import About from "../Views/About";
+import Home from "../Views/Home";
+import ProjectView from "../Views/ProjectView";
+import Projects from "../Views/Projects";
+
+export default function Router() {
+    
+    return (
+        //add a route for ProjectView when you get the firebase storage up and running
+        <div>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/about" element={<About />} />
+                <Route exact path="/projects" element={<Projects />} />
+            </Routes>
+        </div>
+    )
+}
---
+++ src/Views/ProjectView.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ProjectView() {
+    return (
+        <h1>ProjectView</h1>
+    )
+}
