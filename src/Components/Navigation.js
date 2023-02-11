43642619f151516a2822708cfa1ffe07c610f256
Nathan Blackman <nathanpgblackman@gmail.com>
2023-02-11 12:13:55 -0600

    changes on about page as well as a few other tweeks

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Navigation() {
   return (
@@ -15,17 +14,6 @@ export default function Navigation() {
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/projects">Projects</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/Views/About.js
+++ src/Views/About.js
@@ -14,12 +14,16 @@ export default function About() {
     return (
         <div>
             <h1 className="AboutName">Nathan Blackman</h1>
-            <img 
-                className="AboutPic"
-                alt="Nathan Blackman"
-                src={NBPicture}
-            />
-            <p className="AboutDescription">Nathan is a recent graduate of the Nashville Software School. Prefering a job in front end development but educated in both back end development and full stack.</p>
+            <div className="AboutPicNDesc">
+                <img 
+                    className="AboutPic"
+                    alt="Nathan Blackman"
+                    src={NBPicture}
+                />
+                <p className="AboutDescription">Nathan is a recent graduate of the Nashville Software School. Very eager to continue my software journey as well as continue to gain professional experience. I enjoy art, video production, music, and especially technology! Feel free to contact me through the methods below, I would love to hear from you!</p>
+            </div>
+            
+
             <div className='aboutLinks'>
                 <a href="https://github.com/NathanBlackman">Github</a>
                 <a href="https://www.linkedin.com/in/nathan-blackman-979344200/">LinkedIn</a>
